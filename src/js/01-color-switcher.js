0885c9c35f59273db24edc4c46f3add9974f1d2a
gavrila0909 <gavriladanna@gmail.com>
2024-02-07 07:51:21 +0000

    done task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let colorChangeInterval;
 
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+function startColorChange () {
+    startButton.disable = true;
+
+    colorChangeInterval = setInterval(() =>{
+       document.body.style.background = getRandomHexColor();
+    }, 1000);
+}
+
+
+function stopColorChange (){
+    startButton.disable = false;
+    clearInterval(colorChangeInterval);
+    colorChangeInterval = null;
+}
+
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
